ce777c1c236608cacd3a766e9a75abac53df0773
Eric Briscoe <eric.j.briscoe@gmail.com>
2014-08-14 12:46:35 -0700

    Updates for 1.3.0 Draft

--- versions/v1.3.0/channels/map.feature.plot.batch.js
+++ versions/v1.3.0/channels/map.feature.plot.batch.js
@@ -5,9 +5,9 @@ cmapi.channel["map.feature.plot.batch"] = {
     "description": "Plots a batch of feature data on the map.",
     "type": "object",
     "properties": {
-      "payloads": {
-        "description": "array of map.feature.plot payload objects.  See map.feature.plot for details",
-        "type": "array"
+      "features": {
+        "description" : "An array of map.feature.plot payloads.  See map.feature.plot for the object format and schema",
+        "type": "Array"
       },
       "overlayId": {
         "description": "The ID of the overlay this feature should be loaded into. If an overlay with this ID already exists, the new feature is merged into existing overlay; otherwise, a new overlay is created. If no overlayId is included, default overlay with ID equal to sending widget’s ID is used. If an overlay exists, it will retain its status (whether visible or hidden). If an overlay is created, it will be made visible.",
@@ -18,16 +18,13 @@ cmapi.channel["map.feature.plot.batch"] = {
         "description": "Valid values are “true” or “false”. If “true”, then the end user MUST NOT be able to edit the feature from the map’s user interface, if “false” the end user MAY edit the feature from the map’s user interface. Default value is “true”.   If an edit takes place, the map SHOULD dispatch a map.feature.plot with the updated feature to ensure other widgets are aware that a change took place.",
         "type": "boolean",
         "default": true
-      },
-      "messageId": {
-        "description": "A globally unique ID that identifies a particular message.  If sent in a request, the map widget MUST respond with a map.message.complete message (see map.message.complete channel) using this messageId to indicate which batch the map.message.complete message applies to.  This message will maintain the id throughout the lifetime of the message until a map.message.complete is called.",
-        "type": "string"
       }
     },
     "required": ["features"]
   },
   notes: [
-    'Feature object properties are defined in map.feature.plot ',
-    'For overlayId and readOnly when included at the batch level, these values SHALL be applied to all feature objects in the features array that don’t include the corresponding element i.e. similar behavior to CSS.  See map.feature.plot for definitions of these properties. '
+    'If using the channel shouter to send a feature, embedded quotes in KML MUST be escaped with a backward slash (that is, use \\" instead of ").',
+    'If sending GeoJSON follow the guidance in Appendix B for style information.',
+    'When plotting an Area of Interest, the format MUST be “geojson” AND the aoi object defined in Appendix B MUST be included as part of the GeoJSON parameters object.'
   ]
 };
--- versions/v1.3.0/channels/map.feature.unplot.batch.js
+++ versions/v1.3.0/channels/map.feature.unplot.batch.js
@@ -1,27 +1,26 @@
 cmapi.channel["map.feature.unplot.batch"] = {
-    schema: {
-        "$schema": "http://json-schema.org/draft-04/schema#",
-        "title": "map.feature.unplot.batch",
-        "description": "Removes multiple features from the map.",
-        "type": "object",
-        "properties": {
-                  "overlayId": {
-                    "description": "The ID of the overlay where the feature to be removed is found. If no overlayId is included, default overlay with an ID equal to sending widget's ID is assumed.",
-                    "type": "string",
-					"default": "sending widget's ID"
-                },
-                "payloads": {
-                    "description": "array of map.feature.unplot payload objects.  See map.feature.unplot for details",
-                    "type": "array"
-                },
-                "messageId" : {
-                    "description" : "A globally unique ID that identifies a particular message.  If sent in a request, the map widget MUST respond with a map.message.complete message (see map.message.complete channel) using this messageId to indicate which batch the map.message.complete message applies to.  This message will maintain the id throughout the lifetime of the message until a map.message.complete is called.",
-                    "type" : "string"
-                }
-        },
-        "required": ["payloads"]
+  schema: {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "title": "map.feature.unplot.batch",
+    "description": "Plots a batch of feature data on the map.",
+    "type": "object",
+    "properties": {
+      "payloads": {
+        "description" : "An array of map.feature.unplot payloads.  See map.feature.unplot for the object format and schema",
+        "type": "Array"
+      },
+      "overlayId": {
+        "description": "when included at the array level, this value will be applied to all map.feature.unplot objects in the features array that don’t include the corresponding element . I.e., similar behavior to CSS.  See map.feature.unplot for definition of this property",
+        "type": "string",
+        "default": "sending widget's ID"
+      },
+      "messageId": {
+        "description": "A globally unique ID that identifies a particular message.  If the messageId property exists on a message, maps that support the user manipulation extension MUST respond with map.message.complete and map.message.progress messages defined in the User Manipulation extension using the messageId to indicate which message the complete or progress message applies to.  Messages will maintain the same id throughout the lifetime of the message. The messageId can also be used for the map.message.cancel channel defined in the User Manipulation Extension.",
+        "type": "string"
+      }
     },
-    notes: []
-}
-
-
+    "required": ["payloads"]
+  },
+  notes: [
+    ]
+};
